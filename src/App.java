e0210863643ffa19c75ee0a7093b69dcb05975d1
Luis <lf.mc.almeida@poli.ufrj.br>
2023-01-11 14:21:30 -0300

    multiplicador

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String numero = "10";
+        String nome = "Tabela de multiplicação de";
+        String tudo = nome + " " + numero;
+        System.out.println(tudo);
+    }
+}
